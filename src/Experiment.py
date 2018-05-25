50fdf90018899934993f4734bbd8dd6fafba65ab
Nicolas Roulet <nicoroulet@gmail.com>
2018-05-25 16:05:52 -0300

    Move UNet to Models.py, add experiment visualization, add epoch count to checkpoints

--- src/Experiment.py
+++ src/Experiment.py
@@ -1,25 +1,45 @@
+"""Execution routines."""
+
 import Datasets
+import Helpers
 import Metrics
 import Models
-import UNet
 
+import glob
 import os
 
-from keras.callbacks import ModelCheckpoint  # , TensorBoard
-
-patch_shape = (32, 32, 32)
-net_depth = 4
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.metrics import sparse_categorical_accuracy
 
 atlas = Datasets.ATLAS()
 brats = Datasets.BraTS()
 mrbrains = Datasets.MRBrainS()
+tasks = [
+         {"name": "tumor",
+          "labels": ["necrosis",
+                     "edema",
+                     "nonenhancing tumor",
+                     "enhancing tumor"]},
+         {"name": "anatomical",
+          "labels": ["CSF",
+                     "White matter",
+                     "Gray matter"]}
+        ]
 
 
-def train_unet():
+def train_unet(dataset, epochs=1, steps_per_epoch=20, batch_size=5,
+               patch_shape=(32, 32, 32), net_depth=4, n_channels=1):
   """Build UNet, load the weights (if any), train, save weights."""
-  dataset = mrbrains
-  savedir = 'weights'
-  savefile = savedir + '/unet.h5'
+  savedir = 'checkpoints/unet_%s_%d' % (dataset.name, net_depth)
+  weights_file = '%s/weights.h5' % savedir
+  epoch_file = '%s/last_epoch.txt' % savedir
+
+  if os.path.isfile(epoch_file):
+    initial_epoch = int(open(epoch_file, 'r').readline())
+  else:
+    initial_epoch = 0
+
+  epochs += initial_epoch
 
   # tr_gen, val_gen = dataset.get_generators(patch_shape,
   #                                        patch_multiplicity=2**net_depth)
@@ -27,66 +47,89 @@ def train_unet():
 
   n_classes = dataset.n_classes
 
-  model = UNet.UNet(n_classes, depth=net_depth)
+  model = Models.UNet(n_classes, depth=net_depth, n_channels=n_channels)
 
-  model.compile(loss='sparse_categorical_crossentropy',
-                optimizer='sgd',
-                metrics=['accuracy',
-                         Metrics.continuous.sparse_dice_coef,
-                         Metrics.continuous.mean_dice_coef(),
-                         Metrics.discrete.to_continuous(Metrics.discrete.
-                                                        sparse_dice_coef),
-                         Metrics.discrete.to_continuous(Metrics.discrete.
-                                                      mean_dice_coef(n_classes))
+  model.compile(
+                # loss='sparse_categorical_crossentropy',
+                loss=Metrics.ContinuousMetrics.mean_dice_loss(),
+                optimizer='adam',
+                # optimizer=keras.optimizers.Adam(lr=.005),
+                metrics=[sparse_categorical_accuracy,
+                         Metrics.ContinuousMetrics.dice_coef,
+                         Metrics.ContinuousMetrics.mean_dice_coef(),
+                         Metrics.DiscreteMetrics.mean_dice_coef(n_classes)
                          ])
 
   print(model.summary(line_length=150, positions=[.25, .55, .67, 1.]))
 
-  if not os.path.exists(savedir):
-    os.mkdir(savedir)
-  if os.path.exists(savefile):
-    model.load_weights(savefile)
+  if os.path.exists(weights_file):
+    model.load_weights(weights_file)
 
-  model_checkpoint = ModelCheckpoint(savefile,
+  if not os.path.exists(savedir):
+    os.makedirs(savedir)
+  model_checkpoint = ModelCheckpoint(weights_file,
                                      monitor='loss',
                                      save_best_only=True)
   # model_checkpoint = Callback()
-  # tensor_board = TensorBoard(log_dir='./tensorboard',
-  #                            histogram_freq=0,
-  #                            write_graph=True,
-  #                            write_images=True)
 
-  model.fit_generator(tr_gen.generate_batches(batch_size=5),
-                      steps_per_epoch=20,
-                      epochs=2,
+  for file in glob.glob('tensorboard/*'):
+    os.remove(file)
+  tensorboard = TensorBoard(log_dir='./tensorboard',
+                            histogram_freq=0,
+                            write_graph=True,
+                            write_images=True)
+
+  model.fit_generator(tr_gen.generate_batches(batch_size=batch_size),
+                      steps_per_epoch=steps_per_epoch,
+                      initial_epoch=initial_epoch,
+                      epochs=epochs,
                       # validation_data=val_gen.generate_batches(batch_size=1),
                       # validation_steps=5,
-                      callbacks=[model_checkpoint])
+                      callbacks=[model_checkpoint, tensorboard])
 
+  open(epoch_file, 'w').write(str(epochs))
   print("Done")
 
 
-def train_multiunet(epochs=1, steps_per_epoch=20, batch_size=5):
+def visualize_unet(dataset, net_depth=4, n_channels=1):
+  """Compute one MultiUNet prediction and visualize against ground truth."""
+  import numpy as np
+  generator = dataset.get_train_generator((128, 128, 128))
+  generator = generator.generate_batches(batch_size=1)
+
+  n_classes = dataset.n_classes
+
+  model = Models.UNet(n_classes, depth=net_depth, n_channels=n_channels)
+
+  model.compile(loss='sparse_categorical_crossentropy',
+                optimizer='sgd')
+
+  savedir = 'checkpoints/unet_%s_%d' % (dataset.name, net_depth)
+  weights_file = '%s/weights.h5' % savedir
+  if os.path.exists(weights_file):
+    model.load_weights(weights_file)
+
+  x, y = next(generator)
+  while (np.sum(y) < 500):
+    x, y = next(generator)
+  y_pred = np.argmax(model.predict(x), axis=-1)
+  print(set(y_pred.flat))
+  y = y.reshape(y.shape[:-1])
+  Helpers.visualize_predictions(x[0, ..., 0], y[0], y_pred[0])
+
+
+def train_multiunet(epochs=1, steps_per_epoch=20, batch_size=3,
+                    patch_shape=(64, 64, 64)):
   """Build a MultiUNet, load weights (if any), train and save weights.
 
   Args:
-      epochs (int, optional): Description
-      steps_per_epoch (int, optional): Description
-      batch_size (int, optional): Description
+      epochs (int, optional): number of training epochs
+      steps_per_epoch (int, optional): number of batches per epoch
+      batch_size (int, optional): size of training batches
   """
   tumor_gen = brats.get_train_generator(patch_shape)
 
-  anatomical_gen = mrbrains.get_train_generator(patch_shape)
-
-  tasks = [{"name": "anatomical",
-            "labels": ["CSF",
-                       "White matter",
-                       "Gray matter"]},
-           {"name": "tumor",
-            "labels": ["necrosis",
-                       "edema",
-                       "nonenhancing tumor",
-                       "enhancing tumor"]}]
+  # anatomical_gen = mrbrains.get_train_generator(patch_shape)
 
   model = Models.MultiUNet(tasks)
 
@@ -94,28 +137,48 @@ def train_multiunet(epochs=1, steps_per_epoch=20, batch_size=5):
                       tumor_gen.generate_batches(batch_size=batch_size),
                       steps_per_epoch=steps_per_epoch,
                       epochs=epochs)
-  model.fit_generator("anatomical",
-                      anatomical_gen.generate_batches(batch_size=batch_size),
-                      steps_per_epoch=steps_per_epoch,
-                      epochs=epochs)
+  # model.fit_generator("anatomical",
+  #                     anatomical_gen.generate_batches(batch_size=batch_size),
+  #                     steps_per_epoch=steps_per_epoch,
+  #                     epochs=epochs)
 
 
 def test_multiunet():
+  """Build a MultiUNet, load weights and evaluate on generator."""
   # Using the train generator to fit into memory
-  anatomical_gen = mrbrains.get_train_generator(patch_shape)
-
-  tasks = [{"name": "tumor",
-            "labels": ["necrosis",
-                       "edema",
-                       "nonenhancing tumor",
-                       "enhancing tumor"]},
-           {"name": "anatomical",
-            "labels": ["CSF",
-                       "White matter",
-                       "Gray matter"]}]
+  anatomical_gen = mrbrains.get_train_generator((64, 64, 64)).generate_batches()
 
   model = Models.MultiUNet(tasks)
 
-  model.evaluate_generator(anatomical_gen)
+  print(model.evaluate_generator(anatomical_gen,
+                                 [Metrics.discrete.accuracy,
+                                  Metrics.discrete.dice_coef]))
 
-train_multiunet(epochs=5, steps_per_epoch=50)
+
+def visualize_multiunet():
+  """Compute one MultiUNet prediction and visualize against ground truth."""
+  import numpy as np
+  generator = brats.get_train_generator((128, 128, 128))
+  generator = generator.generate_batches(batch_size=1)
+
+  model = Models.MultiUNet(tasks)
+
+  x, y = next(generator)
+  while (np.sum(y) == 0):
+    x, y = next(generator)
+  y_pred = model.predict(x)
+  print(set(y_pred.flat))
+  y_pred[y_pred == 8] = 0
+  y_pred[y_pred == 6] = 0
+  y_pred[y_pred == 7] = 0
+  y = y.reshape(y.shape[:-1])
+  Helpers.visualize_predictions(x[0, ..., 0], y[0], y_pred[0])
+
+# train_unet(mrbrains, epochs=10, steps_per_epoch=10)
+# visualize_unet(mrbrains)
+# train_unet(brats, epochs=40, steps_per_epoch=50, n_channels=4)
+# visualize_unet(brats, n_channels=4)
+
+# train_multiunet(epochs=200, steps_per_epoch=50)
+# test_multiunet()
+# visualize_multiunet()
--- src/Models.py
+++ src/Models.py
@@ -6,10 +6,97 @@ import Metrics
 
 import numpy as np
 
-import UNet
-
-
+from keras.layers import Input, Conv3D, Conv3DTranspose, MaxPooling3D, \
+                         concatenate
+from keras.models import Model
 from keras.callbacks import ModelCheckpoint
+
+
+def build_unet(n_classes, depth=4, base_filters=32, n_channels=1):
+  """ Build a UNet neural network and returns the input and output layers.
+
+  Args:
+      n_classes (int): number of output classes (segmentation labels).
+      depth (int, optional): number of UNet levels. This is not the number
+          of layers of the neural network, a UNet layer is the combination of
+          convolution layers and maxpooling/upconvolution layers.
+      base_filters (int, optional): number of filters on the initial layers.
+          The number of filters is doubled on each maxpooling and halved on
+          each upconvolution.
+  Return:
+    inputs, outputs: input and output layers.
+  """
+  conv_params = {
+      'kernel_size': (3, 3, 3),
+      'strides': (1, 1, 1),
+      'padding': 'same',  # TODO: consider 'valid' (no padding),
+                          # as suggested by paper.
+      'activation': 'relu',
+      # TODO: experiment with regularizers and initializers.
+      'kernel_initializer': 'he_normal'
+      # 'kernel_regularizer': keras.regularizers.l2(.0001)
+  }
+
+  inputs = Input((None, None, None, n_channels))
+  x = inputs
+
+  n_filters = base_filters
+
+  # Layers tat will be used in up-convolution
+  layer_outputs = []
+  # TODO: try adding batch normalization layers.
+
+  # Convolution layers
+  for layer in range(depth - 1):
+    x = Conv3D(filters=n_filters, **conv_params)(x)
+    x = Conv3D(filters=n_filters, **conv_params)(x)
+    layer_outputs.append(x)
+    x = MaxPooling3D(pool_size=(2, 2, 2))(x)
+    n_filters *= 2
+
+  # Bottom layers
+  x = Conv3D(filters=n_filters, **conv_params)(x)
+  x = Conv3D(filters=n_filters, **conv_params)(x)
+
+  # Transposed Convolution layers (up-convolution)
+  for layer in reversed(range(depth - 1)):
+    n_filters //= 2
+    x = Conv3DTranspose(filters=n_filters, kernel_size=(2, 2, 2),
+                        strides=(2, 2, 2))(x)
+    x = concatenate([x, layer_outputs.pop()])
+    x = Conv3D(filters=n_filters, **conv_params)(x)
+    x = Conv3D(filters=n_filters, **conv_params)(x)
+
+  # Final layer
+  x = Conv3D(filters=n_classes, kernel_size=(1, 1, 1), padding='same',
+             activation='softmax')(x)
+
+  return inputs, x
+
+
+class UNet(Model):
+  """UNet model, taken from https://arxiv.org/abs/1505.04597."""
+
+  def __init__(self, n_classes, depth=4, base_filters=32, n_channels=1):
+    """Create UNet model.
+
+    Args:
+        n_classes (int): number of output classes (segmentation labels).
+        depth (int, optional): number of UNet levels. This is not the number
+            of layers of the neural network, a UNet layer is the combination of
+            convolution layers and maxpooling/upconvolution layers.
+        base_filters (int, optional): number of filters on the initial layers.
+            The number of filters is doubled on each maxpooling and halved on
+            each upconvolution.
+    """
+    inputs, outputs = build_unet(n_classes,
+                                 depth=depth,
+                                 base_filters=base_filters,
+                                 n_channels=n_channels)
+    super(UNet, self).__init__(inputs, outputs)
+    self.n_classes = n_classes
+    self.depth = depth
+    self.base_filters = base_filters
 
 
 class MultiUNet:
@@ -116,11 +203,12 @@ class MultiUNet:
             function that takes y_true, y_pred and returns a value
         steps (5, optional): number of evaluation steps.
     """
-    metrics = []
+    metric_values = []
     for i, (x, y_true) in zip(range(steps), generator):
+      y_true = y_true.reshape(y_true.shape[:-1])
       y_pred = self.predict(x)
-      metrics.append([metric(y_true, y_pred) for metric in metrics])
-    return np.mean(metrics, axis=0)
+      metric_values.append([metric(y_true, y_pred) for metric in metrics])
+    return np.mean(metric_values, axis=0)
 
   def predict(self, x):
     """ Return a prediction for the given input.
@@ -130,5 +218,6 @@ class MultiUNet:
     """
     predictions = {}
     for task_name in self.task_names:
-      predictions[task_name] = self.nets[task_name].predict(x)
+      predictions[task_name] = np.argmax(self.nets[task_name].predict(x),
+                                         axis=-1)
     return self.merge_segmentations(predictions)
