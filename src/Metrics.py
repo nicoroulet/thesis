aa77165447a0312f25e52c3f5793a836019d16ee
Nicolas Roulet <nicoroulet@gmail.com>
2018-07-25 01:06:47 -0300

    improved selective crossentropy and crossentropy dice

--- src/Metrics.py
+++ src/Metrics.py
@@ -194,7 +194,7 @@ def selective_sparse_categorical_crossentropy(y_true, y_pred):
   y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
   y_pred_log = K.log(y_pred)
   not_ignore_mask = K.one_hot(K.squeeze(K.cast(y_true, 'int32'), axis=-1),n_classes)
-  loss = -K.sum(K.one_hot(K.squeeze(K.cast(y_true, 'int32'), axis=-1),n_classes) * y_pred_log)
+  loss = -K.sum(not_ignore_mask * y_pred_log)
   return loss
 
 
@@ -214,30 +214,32 @@ def improved_selective_sparse_categorical_crossentropy(y_true, y_pred):
       y_pred (tensor): predicted segmentation scores.
 
   """
-  y_pred_shape = K.shape(y_pred)
-
   n_classes = K.int_shape(y_pred)[-1]
-  batch_size = y_pred_shape[0]
-  y_true = K.reshape(y_true, (batch_size, -1, 1))
-  y_pred = K.reshape(y_pred, (batch_size, -1, n_classes))[..., 1:]  # Drops bg scores.
   y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
   y_pred_log = K.log(y_pred)
+  not_ignore_mask = K.one_hot(K.squeeze(K.cast(y_true, 'int32'), axis=-1),n_classes)
+  crossentropy = -K.mean(not_ignore_mask * y_pred_log)
 
-
+  batch_size = K.shape(y_pred)[0]
+  y_true = K.reshape(y_true, (batch_size, -1, 1))
+  y_pred = K.reshape(y_pred, (batch_size, -1, n_classes))[..., 1:]  # Drops bg scores.
   positive_mask = K.one_hot(K.squeeze(K.cast(y_true, 'int32'), axis=-1), n_classes)[..., 1:]
-  good_scores = -K.sum(positive_mask * y_pred_log)
-
   # labels_mask[i, j] stores whether the ground truth patch i contains label j.
   labels_mask = K.cast(K.any(positive_mask, axis=1), 'float32')
   # not_ignore_mask = K.one_hot(K.squeeze(K.cast(y_true, 'int32'), axis=-1), n_classes)
   ignore_mask = K.cast(K.equal(y_true, -1), 'float32')
   ignore_mask = K.repeat_elements(ignore_mask, n_classes - 1, -1)
-  bad_scores = K.sum(-K.sum(ignore_mask * K.log(1 - y_pred), axis=1) * labels_mask)
+  ignored_fp = K.mean(-K.mean(ignore_mask * K.log(1 - y_pred), axis=1) * labels_mask)
 
   # good_scores = K.print_tensor(good_scores, message='good scores = ')
   # bad_scores = K.print_tensor(bad_scores, message='bad scores = ')
-  loss = (bad_scores + good_scores)
+  loss = (crossentropy + ignored_fp)
   return loss
+
+
+def crossentropy_dice(y_true, y_pred):
+  return (improved_selective_sparse_categorical_crossentropy(y_true, y_pred) * 100 *
+          selective_dice_loss(y_true, y_pred))
 
 
 ################################################################################
